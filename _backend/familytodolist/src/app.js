d91e706a168812e0ba2caa87678b85b040f5fa38
Nusbeisser <nusbeisser@gmail.com>
2022-01-05 15:13:36 +0100

    up

--- _backend/familytodolist/src/app.js
+++ _backend/familytodolist/src/app.js
@@ -20,7 +20,7 @@ app.use(morgan('combined'));
 app.use(
   cors({
     credentials: true,
-    origin: 'https://compassionate-poincare-4250a6.netlify.app/',
+    origin: 'https://compassionate-poincare-4250a6.netlify.app',
   }),
 );
 passport.use(new LocalStrategy(User.authenticate()));
